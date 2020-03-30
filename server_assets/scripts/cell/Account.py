92d1b8af34e51b2cddd83bbeefc0129aed4de165
kgho <kuanggh@outlook.com>
2020-03-30 22:13:26 +0800

    解决报错

--- server_assets/scripts/cell/Account.py
+++ server_assets/scripts/cell/Account.py
@@ -4,13 +4,13 @@ from KBEDebug import *
 from CHAT_INFO import TChatInfo
 import time
 
-class Consumer(KBEngine.Entity):
+class Account(KBEngine.Entity):
 	def __init__(self):
 		KBEngine.Entity.__init__(self)
 
 	def Say(self, Msg):
 	#被客户端调用的说话方法
 		DEBUG_MSG("Account[%i] Say %s" % (self.id, Msg))
-		Props = {"Name" : self.__ACCOUNT_NAME__, "Time" : time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()), "Msg" : BackMsg}
+		Props = {"Name" : "User" + str(self.id), "Time" : time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()), "Msg" : Msg}
 		ChatInfo = TChatInfo().createFromDict(Props)
 		self.allClients.OnSay(ChatInfo)
