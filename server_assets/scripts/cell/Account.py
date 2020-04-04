883a6e7ea8b96c1a7ba74585cf67f7b4f11a27c6
kgho <kuanggh@outlook.com>
2020-04-04 15:55:33 +0800

    Bug Fix
    看不到对方玩家生成

--- server_assets/scripts/cell/Account.py
+++ server_assets/scripts/cell/Account.py
@@ -7,6 +7,8 @@ import time
 class Account(KBEngine.Entity):
 	def __init__(self):
 		KBEngine.Entity.__init__(self)
+		#添加定时器(第一次延迟时间20分钟后，循环延迟时间0不循环，传给延迟方法的参数）
+		#self.addTimer(20,0,0)
 
 	def Say(self, Msg):
 	#被客户端调用的说话方法
@@ -14,3 +16,14 @@ class Account(KBEngine.Entity):
 		Props = {"Name" : "User" + str(self.id), "Time" : time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()), "Msg" : Msg}
 		ChatInfo = TChatInfo().createFromDict(Props)
 		self.allClients.OnSay(ChatInfo)
+
+	def onTimer(self, tid, userArg):
+		"""
+		:param tid:计时器ID
+		:param userArg:自定义数据
+		"""
+		#日志打印
+		DEBUG_MSG("Account[%i] onTimer %i" % (self.id, userArg))
+		if userArg is 0:
+			#销毁这个cell实体，但不会销毁base实体
+			self.destroy()
