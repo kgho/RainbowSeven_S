9caae51b740e8d739624e91b90736031ba4d7ad3
kgho <kuanggh@outlook.com>
2020-04-05 21:54:09 +0800

    测试服务器触发器

--- server_assets/scripts/cell/SRoom.py
+++ server_assets/scripts/cell/SRoom.py
@@ -1,6 +1,10 @@
 import KBEngine
 from KBEDebug import *
+import Math #KBE Vector的包
 
 class SRoom(KBEngine.Space):
 	def __init__(self):
 		KBEngine.addSpaceGeometryMapping(self.spaceID, None, "spaces/GameMap")
+
+		#创建传送门
+		KBEngine.createEntity("Gate", self.spaceID, Math.Vector3(0,0,0), Math.Vector3(0,0,0), {})
