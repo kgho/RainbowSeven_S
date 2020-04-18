0b477303e280eba87b051446c684c168b4904849
kgho <kuanggh@outlook.com>
2020-04-18 10:52:02 +0800

    0001_账户没有干员自动解锁 001 干员

--- server_assets/scripts/base/Account.py
+++ server_assets/scripts/base/Account.py
@@ -54,14 +54,21 @@ class Account(KBEngine.Proxy):
         """
         DEBUG_MSG("Account[%i]::ReqRoleList: Size=%i." %(self.id, len(self.RoleList)))
 
-        self.client.OnReqRoleList(self.RoleList)
+        if(len(self.RoleList)>0):
+            self.client.OnReqRoleList(self.RoleList)
+        else:
+            # 如果该账户没有角色自动解锁第一个干员
+            DEBUG_MSG("Account[%i]::ReqRoleList:Unlock Role 001." %(self.id))
+            self.ReqUnlockRole(1)
+            DEBUG_MSG("Account[%i]::ReqRoleList: Size=%i." %(self.id, len(self.RoleList)))
+            self.client.OnReqRoleList(self.RoleList)
 
-    def ReqUnlockole(self, RoleType):
+    def ReqUnlockRole(self, RoleType):
         """
         客户端请求解锁干员
         RoleType : 干员类型
         """
-        DEBUG_MSG("Account[%i]::ReqUnlockole: RoleType=%i" % (self.id, RoleType))
+        DEBUG_MSG("Account[%i]::ReqUnlockRole: RoleType=%i" % (self.id, RoleType))
 
         # 判断该类型干员是否已经解锁
         # 0:干员已解锁
@@ -69,12 +76,12 @@ class Account(KBEngine.Proxy):
             # 已存在 并 已解锁
             if info[1] == RoleType and info[2] == 1:
                 self.client.OnReqUnlockRole(1)
-                DEBUG_MSG("Account[%i]::ReqUnlockole: RoleType=%i Has Unlock." % (self.id, RoleType))
+                DEBUG_MSG("Account[%i]::ReqUnlockRole: RoleType=%i Has Unlock." % (self.id, RoleType))
                 return
             # 与存在 并 未解锁，解锁后返回
             if info[1] == RoleType and info[2] == 0:
                 self.client.OnReqUnlockRole(2)
-                DEBUG_MSG("Account[%i]::ReqUnlockole: RoleType=%i Unlock Angain." % (self.id, RoleType))
+                DEBUG_MSG("Account[%i]::ReqUnlockRole: RoleType=%i Unlock Angain." % (self.id, RoleType))
                 return
 
         # 创建Role， 设置角色初始变量
@@ -96,50 +103,45 @@ class Account(KBEngine.Proxy):
             # 创建Role失败
             self.client.OnReqUnlockRole(3)
 
-        def _OnRoleSaved(self, Success, Role):
-            """
-            新建角色写入数据库回调
-            Success : 是否写入成功
-            Role : 写入数据库实体
-            """
-
-            # 如果账号已销毁，把角色从数据库删除
-            if self.isDestroyed:
-                if Role:
-                    # destroy方法内填入Ture会从数据库删除实体数据
-                    Role.destroy(True)
-                return
-            
-            # 回调函数返回参数 角色
-            RoleInfo = TRoleInfo()
-            RoleInfo.extentd([0, 0, 1, 0, 0, 0, 0, 0])
-
+    def _OnRoleSaved(self, Success, Role):
+        """
+        新建角色写入数据库回调
+        Success : 是否写入成功
+        Role : 写入数据库实体
+        """
+        # 如果账号已销毁，把角色从数据库删除
+        if self.isDestroyed:
             if Role:
-                # cellData可以获取未生成cell实体时cell作用域的变量
-                RoleInfo[1] = Role.cellData["RoleType"]
-                #判断是否写入数据库成功
-                if Success:
-                    RoleInfo[0] = Role.databaseID
-                    # 将 Role 填入 RoleList
-                    self.RoleList[Role.databaseID] = RoleInfo
-                    # 通知客户端 解锁成功
-                    if self.cient:
-                        # 说明Role 新解锁干员信息 写入数据库成功
-                        self.client.OnReqUnlockRole(0)
-                    # 保存到数据库
-                    self.writeToDB()
-                else:
-                    if self.client:
-                        # 说明写入失败
-                        self.client.OnReqUnlockRole(4)
-
-                # 销毁Role
-                Role.destroy()
-
+                # destroy方法内填入Ture会从数据库删除实体数据
+                Role.destroy(True)
+            return
+        
+        # 回调函数返回参数 角色
+        RoleInfo = TRoleInfo()
+        RoleInfo.extend([0, 0, 1, 0, 0, 0, 0, 0])
+        if Role:
+            # cellData可以获取未生成cell实体时cell作用域的变量
+            RoleInfo[1] = Role.cellData["RoleType"]
+            #判断是否写入数据库成功
+            if Success:
+                RoleInfo[0] = Role.databaseID
+                # 将 Role 填入 RoleList
+                self.RoleList[Role.databaseID] = RoleInfo
+                # 通知客户端 解锁成功
+                if self.client:
+                    # 说明Role 新解锁干员信息 写入数据库成功
+                    self.client.OnReqUnlockRole(0)
+                # 保存到数据库
+                self.writeToDB()
             else:
                 if self.client:
-                    # 解锁失败
-                    self.client.self.client.OnReqUnlockRole(3)
-
+                    # 说明写入失败
+                    self.client.OnReqUnlockRole(4)
+            # 销毁Role
+            Role.destroy()
+        else:
+            if self.client:
+                # 解锁失败
+                self.client.self.client.OnReqUnlockRole(3)
 
 
